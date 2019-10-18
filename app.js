b35a919d0237ebfe4bd89ae1111c0a1b67094082
kdomgjoni <domgjonikrist@gmail.com>
2019-10-18 06:20:03 -0700

    updating the percentages UI controller

--- app.js
+++ app.js
@@ -147,7 +147,8 @@ var UIController = (function(){
 		incomeLabel: '.budget__income--value',
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
-		container: '.container'
+		container: '.container',
+		expensesPercLabel: '.item__percentage'
 	};
 
 	// Objetxt that are going to returned are public 
@@ -213,6 +214,26 @@ var UIController = (function(){
 			el.parentNode.removeChild(el);
 		},
 
+		displayPercentages: function(percentages){
+			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+			console.log(fields);
+
+			var nodeListForEach = function(list, callback){
+				for(var i = 0; i < list.length; i++){
+					callback(list[i], i);
+				}
+			}
+
+			nodeListForEach(fields, function(current, index){
+				if(percentages[index] > 0){
+					current.textContent = percentages[index] + ' %';	
+				}else{
+					current.textContent = '---';
+				}
+				
+			});
+		},
+
  
 		getDOMstrings: function(){
 			return DOMstrings;
@@ -262,8 +283,7 @@ var controller = (function(budgetCtrl, UICtrl){
 		var percentages = budgetCtrl.getPercentages();
 
 		// 3. Update UI with new percentages
-
-		console.log(percentages);
+		UICtrl.displayPercentages(percentages);
 	}
 
 	var ctrlAddItem = function(){
